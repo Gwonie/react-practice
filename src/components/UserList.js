d02c4c7f9c76f288d16939c63819130a9182c32e
MinJiwon <jwjwmin111@gmail.com>
2022-09-23 11:00:07 +0900

    :art: 배열에 항목 수정
    배열에 항목 수정-map

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -2,21 +2,36 @@ import React from "react";
 
 // UserList > User
 
-function User({ user, onRemove }) {
+function User({ user, onRemove, onToggle }) {
   return (
     <div>
-      <b>{user.username}</b> <span>({user.email})</span>
+      <b
+        style={{
+          cursor: "pointer",
+          color: user.active ? "green" : "black",
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
       <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
 }
 
-function UserList({ users, onRemove }) {
+function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
       {users.map((user) => (
         // User반환
-        <User user={user} key={user.id} onRemove={onRemove} />
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
